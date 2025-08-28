b07b58f0da9e9e86a25c092679028b6c72672313
Maddox Madia <mrm00077@mix.wvu.edu>
2025-08-28 11:16:22 -0400

    Added Practice1

---
+++ Practice/Practice1/Practice1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+/* Name: Maddox Madia 
+* Date: Thursday, 8/28/25
+* In class practice
+* This program does something
+*/
+
+namespace Practice1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, welcome to practice 1!");
+        Console.WriteLine("This portion shows us how to print things out");
+        //################ Functionality 1 ####################
+        Console.WriteLine("You are now in functionality 1");
+        Console.WriteLine("A circle with a radius of 1 is 3.14");
+        //The formula to calculate area of a circle is r * r * 3.14, where r is radius
+        //r stores the radius
+        int theRadius = 10;
+        //a stores the area
+        double theArea = theRadius * theRadius * 3.14;
+
+        String n = "Circle 1";
+        //The following 3 lines show different ways to print out the area
+        //Console.WriteLine("A circle with a radius of " + r + " is " + a");
+        //Console.WriteLine("A circle with a radius of {0} is {1}", r, a);
+        //Console.WriteLine("A circle with a radius of {a} is {r}");
+
+        Console.WriteLine($"{n} with a radius of {theRadius} is {theArea}");
+    }
+}
