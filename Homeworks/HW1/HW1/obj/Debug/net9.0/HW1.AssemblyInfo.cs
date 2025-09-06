1c0eac6f0840fa28973e97c1bd167fb977c55481
Maddox Madia <mrm00077@mix.wvu.edu>
2025-09-05 21:52:30 -0400

    HW 1 uploaded

--- Homeworks/HW1/HW1/obj/Debug/net9.0/HW1.AssemblyInfo.cs
+++ Homeworks/HW1/HW1/obj/Debug/net9.0/HW1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HW1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70abab959078fb79be7d70c10d0b88e2a8e5c92f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e546466fa7ed1bb22189996f98192ba9497461e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HW1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HW1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
