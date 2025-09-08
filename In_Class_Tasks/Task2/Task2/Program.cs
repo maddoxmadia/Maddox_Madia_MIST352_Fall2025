5fe4a6ebae2b3b92178306967bf5a232e4e7069a
Maddox Madia <mrm00077@mix.wvu.edu>
2025-09-08 15:51:21 -0400

    Task2

--- In_Class_Tasks/Task2/Task2/Program.cs
+++ In_Class_Tasks/Task2/Task2/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace Task2;
+﻿/* Name: Maddox Madia
+* Class: MIST 352 Fall 2025
+* 9/8/25
+*This program prompts the user for hero's name and quest
+*/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Task2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Define variables
+        string strHeroName, strFavoritePlace, strLuckyNumberText;
+
+        Console.WriteLine("What is your name?");
+        Console.Write("Hero name:  ");
+        strHeroName = Console.ReadLine().Trim();
+
+        Console.WriteLine("What is your favorite place?");
+        Console.Write("Favorite place:  ");
+        strFavoritePlace = Console.ReadLine().Trim();
+
+        Console.WriteLine("What is your lucky number?");
+        Console.Write("Lucky number:  ");
+        strLuckyNumberText = Console.ReadLine().Trim();
+        bool parseOkay = int.TryParse(strLuckyNumberText, out int LuckyNumber);
+
+        // Line 1 Meet heroName + !
+        string line1 = "Meet" + strHeroName.ToUpper() + "!";
+        // Line 2 Today's quest starts in
+        string line2 = "Today your quest starts in " + strFavoritePlace + ".";
+        // Line 3 "LuckyNumber" + luckynumber
+        string line3 = "Lucky Number: " + strLuckyNumberText;
+        // Build nick
+        string Nick = strHeroName.Length >= 3 ? strHeroName.Substring(0, 3).ToUpper() : strHeroName.ToUpper();
+
+        // Build code
+        string Code = "#" + Nick + "-" + strLuckyNumberText;
+
+        // Build strings + NEWLINE
+        string report = line1 + Environment.NewLine + line2 + Environment.NewLine + line3 + Environment.NewLine + Code;
+
+        // Print report
+        Console.WriteLine(report);
+
+        // Print parse success & parseOkay
+        Console.WriteLine("Parse success:" + parseOkay);
+        Console.WriteLine("Hero length:" + strHeroName.Length);
+        Console.WriteLine("Place contains a space: " + (strFavoritePlace.IndexOf(" ") >= 0));
+
     }
 }
--- In_Class_Tasks/Task2/Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ In_Class_Tasks/Task2/Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e546466fa7ed1bb22189996f98192ba9497461e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0eac6f0840fa28973e97c1bd167fb977c55481")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
